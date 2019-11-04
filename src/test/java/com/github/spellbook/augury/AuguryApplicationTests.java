b2e9a40b8fc9078c1ae5389e74b4cdee3ba18a18
Marcie Harrison <marcie.w.harrison@gmail.com>
2019-11-03 20:03:22 -0600

    Initial commit

---
+++ src/test/java/com/github/spellbook/augury/AuguryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.spellbook.augury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuguryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
